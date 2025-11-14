59244cab7075b63da8dd8bb0a84e9b111eed0d4d
marcoshanono35-dot <marcoshanono35@gmail.com>
2025-11-13 22:21:59 -0500

    Update FirewallAndRules.ps1

--- FirewallAndRules.ps1
+++ FirewallAndRules.ps1
@@ -12,8 +12,32 @@ Set-NetFirewallProfile -Profile Domain,Private,Public `
 Remove-NetFirewallRule
 
 #set rules for scored services (placeholder examples, change as competition requires):
-New-NetFirewallRule -DisplayName "Remote Desktop Protocol" -Direction Inbound -Action Allow -LocalPort 3389
+New-NetFirewallRule -DisplayName "Allow DNS Outbound" -Direction Outbound -Protocol UDP -RemotePort 53 -Action Allow
 
-New-NetFirewallRule -DisplayName "HTTPS" -Direction Outbound -Protocol TCP -Action Allow -RemotePort 443
+New-NetFirewallRule -DisplayName "Allow RDP" -Direction Inbound -Protocol TCP -LocalPort 3389 -Action Allow
 
+New-NetFirewallRule -DisplayName "Allow SMB" -Direction Inbound -Protocol TCP -LocalPort 445,139 -Action Allow
 
+New-NetFirewallRule -DisplayName "Allow Web Traffic" -Direction Inbound -Protocol TCP -LocalPort 80,443 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow NTP Outbound" -Direction Outbound -Protocol UDP -RemotePort 123 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow DHCP Outbound" -Direction Outbound -Protocol UDP -LocalPort 68 -RemotePort 67 -Action Allow
+New-NetFirewallRule -DisplayName "Allow DHCP Inbound"  -Direction Inbound  -Protocol UDP -LocalPort 67 -RemotePort 68 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow DNS Outbound"  -Direction Outbound -Protocol UDP -RemotePort 53 -Action Allow
+New-NetFirewallRule -DisplayName "Allow DNS Responses"  -Direction Inbound  -Protocol UDP -LocalPort 49152-65535 -Action Allow
+New-NetFirewallRule -DisplayName "Allow DNS TCP Outbound" -Direction Outbound -Protocol TCP -RemotePort 53 -Action Allow
+New-NetFirewallRule -DisplayName "Allow DNS TCP Responses" -Direction Inbound -Protocol TCP -LocalPort 49152-65535 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow RPC Endpoint Mapper" -Direction Inbound -Protocol TCP -LocalPort 135 -Action Allow
+New-NetFirewallRule -DisplayName "Allow RPC Dynamic Ports"  -Direction Inbound -Protocol TCP -LocalPort 49152-65535 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow SMB Outbound" -Direction Outbound -Protocol TCP -RemotePort 445 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow NTP Responses" -Direction Inbound -Protocol UDP -LocalPort 49152-65535 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow Windows Update" -Direction Outbound -Protocol TCP -RemotePort 443 -Action Allow
+
+New-NetFirewallRule -DisplayName "Allow Loopback" -Direction Inbound  -LocalAddress 127.0.0.1 -Action Allow
+New-NetFirewallRule -DisplayName "Allow Loopback" -Direction Outbound -RemoteAddress 127.0.0.1 -Action Allow
