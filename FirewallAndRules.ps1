483f84f1a03d384761a7527aac0ea025c07a5fff
marcoshanono35-dot <marcoshanono35@gmail.com>
2025-11-13 21:34:27 -0500

    Update FirewallAndRules.ps1

--- FirewallAndRules.ps1
+++ FirewallAndRules.ps1
@@ -1,3 +1,6 @@
+#create backup
+netsh advfirewall export "C:\fwbackup.wfw"
+
 #set initial firewall state, block all traffic
 Set-NetFirewallProfile -Profile Domain,Private,Public `
 -DefaultInboundAction Block -DefaultOutboundAction Block -EnableStealthModeForIPsec true `
@@ -12,4 +15,5 @@ Remove-NetFirewallRule
 New-NetFirewallRule -DisplayName "Remote Desktop Protocol" -Direction Inbound -Action Allow -LocalPort 3389
 
 New-NetFirewallRule -DisplayName "HTTPS" -Direction Outbound -Protocol TCP -Action Allow -RemotePort 443
+
 
