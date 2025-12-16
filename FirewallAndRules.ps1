5e61ef449fa4bb771e6f93d0a365639d8e06aaca
marcoshanono35-dot <marcoshanono35@gmail.com>
2025-12-16 15:51:57 -0500

    Update FirewallAndRules.ps1

--- FirewallAndRules.ps1
+++ FirewallAndRules.ps1
@@ -45,4 +45,25 @@ New-NetFirewallRule -Profile Domain -DisplayName "Allow SMB Outbound" -Direction
 
 New-NetFirewallRule -Profile Domain -DisplayName "Allow Windows Update" -Direction Outbound -Protocol TCP -RemotePort 443 -Action Allow
 
-New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Echo Request" -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow
+#AD Inbound Rules
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Echo Request ICMPv4" -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Echo Request ICMPv6" -Direction Inbound -Protocol ICMPv6 -IcmpType 128 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - LDAP TCP-in" -Direction Inbound -Protocol TCP -LocalPort 389 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - LDAP UDP-in" -Direction Inbound -Protocol UDP -LocalPort 389 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - LDAP Global TCP-in" -Direction Inbound -Protocol TCP -LocalPort 3268 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - NetBIOS Name Resolution" -Direction Inbound -Protocol UDP -LocalPort 138 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - SAM TCP" -Direction Inbound -Protocol TCP -LocalPort 445 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - SAM UDP" -Direction Inbound -Protocol UDP -LocalPort 445 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - LDAPS" -Direction Inbound -Protocol TCP -LocalPort 636 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - LDAPS" -Direction Inbound -Protocol TCP -LocalPort 3269 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - W32Time" -Direction Inbound -Protocol UDP -LocalPort 123 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - RPC" -Direction Inbound -Protocol TCP -LocalPort 5000-5100 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - RPC-EPMAP" -Direction Inbound -Protocol TCP -LocalPort 135 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Web Services" -Direction Inbound -Protocol TCP -LocalPort 9389 -Action Allow
+
+#AD Outbound Rules
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Echo Request ICMPv4" -Direction Outbound -Protocol ICMPv4 -IcmpType 8 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Echo Request ICMPv6" -Direction Outbound -Protocol ICMPv6 -IcmpType 128 -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - TCP-out" -Direction Outbound -Protocol TCP -LocalPort Any -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - UDP-out" -Direction Outbound -Protocol UDP -LocalPort Any -Action Allow
+New-NetFirewallRule -Profile Domain -DisplayName "ADDC - Web Services TCP-out" -Direction Outbound -Protocol TCP -LocalPort Any -Action Allow
