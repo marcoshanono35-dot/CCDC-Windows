d6ea3270c435332317311371e6a4650c42373188
Marcos Hanono <marcoshanono35@gmail.com>
2026-01-06 18:16:47 -0500

    Update temp.ps1

--- Backups/temp.ps1
+++ Backups/temp.ps1
@@ -1,3 +1,1 @@
-Get-WinEvent -LogName "DNS Server" -MaxEvents 20 | 
-    Where-Object { $_.Id -eq 4000 -or $_.Id -eq 4015 } | 
-    Select-Object TimeCreated, Id, Message | Format-List
+Get-ADObject -Filter * -SearchBase "DC=GREAT,DC=CRETACEOUS" | Group-Object ObjectClass | Select-Object Name, Count
